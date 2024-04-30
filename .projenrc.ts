f3dcc7b8663230769a7ba2f27f0a8cdcca0b04b2
Matthew Rawlings <contact@matthewrawlings.com>
2024-04-29 22:16:31 -0700

    feat: disable publish

--- .projenrc.ts
+++ .projenrc.ts
@@ -5,21 +5,12 @@ const project = new AwlsringAwsCdkLibrary({
   cdkVersion: '2.55.0',
   name: 'cdk-aws-discord-notifiers',
   repositoryUrl: 'https://github.com/awlsring/cdk-aws-discord-notifiers.git',
-  description: 'A package that vends constructs to notify about AWS resources via discord',
-  keywords: [
-    'discord',
-    'notification',
-    'aws',
-    'billing',
-  ],
-  deps: [
-    'constructs@^10.1.52',
-  ],
-  devDeps: [
-    'constructs@10.1.52',
-    'esbuild',
-  ],
-  publish: true,
+  description:
+    'A package that vends constructs to notify about AWS resources via discord',
+  keywords: ['discord', 'notification', 'aws', 'billing'],
+  deps: ['constructs@^10.1.52'],
+  devDeps: ['constructs@10.1.52', 'esbuild'],
+  publish: false,
   bundledDeps: [
     'pino',
     '@types/pino',
